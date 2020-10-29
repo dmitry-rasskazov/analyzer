a308ae93c4ab28c3fb5a8072a01231b5c508e8e3
Dmitry Rasskazov <mitya_myzikant@mail.ru>
2020-10-29 17:40:13 +0300

    Created controller the Analyzer service

---
+++ src/Controller/AnalyzerController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Entity\Result;
+use App\Logic\SequenceAnalyzer;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AnalyzerController extends AbstractController
+{
+    /**
+     * Возвращает результат согласно правилу, из аргументов, предоставленных авторизованным пользователем
+     * @param  Request $request
+     * @param  EntityManagerInterface $entityManager
+     * @param  UserRepository $userRepository
+     * @return JsonResponse
+     * @throw  \Exception
+     * @Route("/setseparator", name="array_separator", methods={"POST"})
+     */
+    public function setSeparator(Request $request, EntityManagerInterface $entityManager, UserRepository $userRepository): JsonResponse
+    {
+        if($request->headers->get('Content-Type') != 'application/json') {
+            return new JsonResponse(['error' => 'Ожидаемый Content-Type - application/json'], 415);
+        }
+        
+        $data = json_decode($request->getContent());
+        try{
+
+            if($data === null) {
+                throw new \Exception('Отсутствуют поля, либо формат данных отличный от JSON');
+            }
+            // Проверка авторизации
+            $user = $userRepository->findOneBy([
+                'name'      => $data->user_name,
+                'auth_code' => $data->auth_code
+            ]);
+
+            if($user === null) {
+                throw new \Exception('Пользователь с таким именем и/или кодом авторизации не существует!');
+            }
+            // Получаем индекс элемента массива, согласно правилу
+            $analyzer = new SequenceAnalyzer($data->sequence);
+            $index    = $analyzer->getIndex();
+            // Сохранение результата в базу
+            $result = new Result();
+            $result->setArr($data->sequence)
+                   ->setNum($data->number)
+                   ->setIndex($index)
+                   ->setUserId($user->getId());
+
+            return new JsonResponse(['index' => $index], 200);
+        } catch (\Exception $e) {
+            return new JsonResponse(['error' => $e->getMessage()], 400);
+        }
+    }
+}
